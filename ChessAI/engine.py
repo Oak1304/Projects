a052b2af6da6903303b705fa60490c5cb8659bb0
Oak1304 <84626339+Oak1304@users.noreply.github.com>
2022-11-23 11:45:48 +0000

    Create engine.py

---
+++ ChessAI/engine.py
@@ -0,0 +1,21 @@
+game = [
+["bR","bN","bB","bQ","bK","bB","bN","bR"],
+["bP","bP","bP","bP","bP","bP","bP","bP"],
+[None,None,None,None,None,None,None,None],
+[None,None,None,None,None,None,None,None],
+[None,None,None,None,None,None,None,None],
+[None,None,None,None,None,None,None,None],
+["wP","wP","wP","wP","wP","wP","wP","wP"],
+["wR","wN","wB","wQ","wK","wB","wN","wR"],
+]
+moves = 0
+possible = [
+[0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0],
+]
